fdb87d96d30b505fbeb1e01476188180552b9334
Norihito Kambe <60634121+nori3636@users.noreply.github.com>
2022-10-29 23:41:58 +0900

    Update src/components/Author.svelte
    Co-authored-by: TOMIKAWA Sotaro <15074382+ssssota@users.noreply.github.com>

--- src/components/Author.svelte
+++ src/components/Author.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   export let author: string;
+  export let size = 24;
 </script>
 
 <a href="https://github.com/{author}" target="_blank">
-  <img width={24} height={24} src="https://github.com/{author}.png?size=24" alt="" />
+  <img width={size} height={size} src="https://github.com/{author}.png?size={size}" alt="" />
   {author}
 </a>
 
